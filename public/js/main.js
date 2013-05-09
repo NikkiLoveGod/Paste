75258913d0a9e2808ead49aa997b0445eb9d2505
Sami Kurvinen <sami.kurvinen@g-works.fi>
2013-05-09 14:34:31 +0300

    Navigation no longer comes up in selection and copy paste

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,9 @@
 jQuery(document).ready(function($) {
 	$('.pastebox').tabby();
+
+    $('.nav a').each(function() {
+        $(this).attr('data-content', $(this).text())
+               .text('');
+    });
+
 });
